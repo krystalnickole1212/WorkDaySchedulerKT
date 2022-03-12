7f5be30d1f842d1d138eae2d0658305aafa5e0c6
krystalnickole1212 <krystalnickole_12@yahoo.com>
2022-03-12 14:30:44 -0600

    added color changes, added local storage, added current time, date, added past, present, future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,61 @@
 //create the elements
 var dateAndTime = document.createElement("currentDay");
-var militaryHour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var currentHour = moment().hours();
 var saveBtn = document.querySelectorAll(".saveBtn");
 
 //display date and time at top of scheduler
-dateAndTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+dateAndTime = moment().format('MMMM Do YYYY, hh:mm:ss a');
 console.log(dateAndTime);
+
 document.getElementById("currentDay").innerHTML = dateAndTime;
 
-document.querySelector("#currentDay").textContent = moment().format("dddd, MMMM Do YYYY");
+document.querySelector("#currentDay").textContent = moment().format("dddd, MMMM Do YYYY, hh:mm a");
 
+
+$(document).ready(function(){
+    //on click, enter event in time block
+    //local storage saves event
+    $(".saveBtn").on("click", function (event) {
+        var dataId = $(event.target).data("id")
+       
+         var text = $(this).closest(".row").find(".description").val()
+        
+         
+        localStorage.setItem(dataId, text);
+        console.log(dataId);
+        console.log(text);
+    })
+})
+
+//var militaryTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 //time block is in the present
-for (var i = 0; i < 9; i++) {
-    if (currentHour === militaryHour[i]) {
-      console.log(i);
-      document.getElementById(i).classList.add("present");
+// get current number of hours
+// loop over time blocks
+$('.description').each(function () {
+    var blockHour = parseInt(
+        $(this)
+            .attr('id')
+    );
+    console.log(currentHour, blockHour)
+
+    //past hours
+    if (blockHour < currentHour) {
+        $(this).addClass("past")
     }
-//time block is in the past
-if (currentHour > militaryHour[i]) {
-    console.log(i);
-    document.getElementById(i).classList.add("past");
-  }
-//time block is in the future
-if (currentHour < militaryHour[i]) {
-    console.log(i);
-    document.getElementById(i).classList.add("future");
-  }
 
-//on click, enter event in time block 
+    //current hours 
 
+    else if (blockHour === currentHour) {
+        $(this).addClass("present")
+        $(this).removeClass("past")
+    }
 
-//local storage saves event 
+    //future hours
 
+    else {
+        $(this).addClass("future")
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+     }
 
-//event persists
+})
